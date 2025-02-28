217478a21ae0426c50146ccd525286d7a3b21b0a
konrad svahn <konrad.svahn@yahoo.com>
2025-02-28 08:15:27 +0200

    marker

--- user/App.js
+++ user/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import {ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View,} from 'react-native';
+import {ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View, WebView} from 'react-native';
 import MapScreen from './app/screens/MapScreen';
-import { MapView } from "@maplibre/maplibre-react-native";
+import { MapView, MarkerView} from "@maplibre/maplibre-react-native";
 
 export default function App() { 
   return (
-    <MapView style={{ flex: 1 }} />
+    <MapView style={{ flex: 1 }} >
+      <MarkerView coordinate={[7.20815, 2.29941]} anchor={{ x: 0.5, y: 0.5}}>
+        <Text>marker</Text>
+      </MarkerView>
+    </MapView>
   );
 }
 
